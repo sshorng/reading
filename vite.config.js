fd5ed1da7f4a18c5abe127b28ffdb3a5512bfb80
sshorng <sshorng@gmail.com>
2026-03-02 22:13:09 +0800

    chore: 清理 build log 檔案，更新 vite 配置

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/reading/',
+  root: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  },
   plugins: [
     vue(),
     tailwindcss(),
   ],
+  server: {
+    fs: {
+      allow: ['src', 'public', 'node_modules']
+    }
+  }
 })
